acb860465f02b2337d9b47e017e6db010048ddc6
Grace <gracelu1011@gmail.com>
2018-12-01 18:26:03 -0800

    add files

---
+++ src/views/Form.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="ui centered card">
+    <h1 class="ui header">Data Input</h1>
+
+<input type="radio" value="One" v-model="picked">
+<input type="radio" value="Two" v-model="picked">
+<span>Picked: {{ picked }}</span>
+
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h1 class="ui header">Welcome</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'home'
+}
+</script>
+
+<style lang="scss">
+
+</style>
